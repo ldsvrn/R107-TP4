d0003fa1307864da2a617a242871102c88693e85
ldsvrn <frais64@orange.fr>
2021-12-08 15:37:19 +0100

    format

--- exo4partie1.py
+++ exo4partie1.py
@@ -3,12 +3,14 @@ L1 = [2, 7, 5, 6, 7, 1, 6, 2, 1, 7, 6]
 counter = 0
 num = 0
 
+
 def listcount(liste, num):
     count = 0
     for i in liste:
         if i == num:
             count += 1
     return count
+
 
 for i in L1:
     j = listcount(L1, i)
